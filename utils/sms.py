93d43363691d8b17d18588e49d0a966f4754f409
Noah Shutty <noahshutty@gmail.com>
2017-03-05 15:44:22 -0800

    finished twilio sms helper

--- utils/sms.py
+++ utils/sms.py
@@ -9,12 +9,17 @@ with open('twilio_config.json', 'r') as infile:
     twilio_config = json.load(infile)
 
 
-# account_sid
-# auth_token
-# phone_number
-
 twilio_client = TwilioRestClient(
     twilio_config['account_sid'],
     twilio_config['auth_token']
 )
 
+def send_sms(to_number, body):
+    message = twilio_client.messages.create(
+        body=body,
+        to=to_number,
+        from_=twilio_config['phone_number']
+    )
+
+if __name__ == '__main__':
+    send_sms('+12485203071', 'hello twilio helper works')
