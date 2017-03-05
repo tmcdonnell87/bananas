7e03621fedf4329ad3db9f11dbf4b628d00534da
Terry McDonnell <terry.mcdonnell@gmail.com>
2017-03-05 15:20:03 -0800

    landing page

--- bananas/appointments/admin.py
+++ bananas/appointments/admin.py
@@ -3,9 +3,11 @@ from .models import Appointment, Document, Message
 
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ('time', 'client_last_name', 'client_phone')
-    #def get_name(self, obj):
-    #    return obj.cousenlor.name
+    search_fields = ('time', 'client_last_name', 'client_phone')
+    list_display = ('time', 'client_last_name', 'client_phone', 'counselor_name')
+    def counselor_name(self, obj):
+        return obj.counselor.name
+    counselor_name.short_description = 'Counselor'
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
