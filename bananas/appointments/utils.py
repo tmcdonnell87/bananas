0ef99efbcd252188ffcc495af1848786869192d7
Danny Eiden <deiden26@Dannys-MacBook-Pro.local>
2017-03-11 12:30:41 -0800

    Create Message Sending Task
    * Move message scheduler into utils
    * Strip out django-cron
    * Fix django settings from env variables
    * Add import .env file to dev.yml
    * Register send_messages command that can be run with manage.py

--- bananas/appointments/utils.py
+++ bananas/appointments/utils.py
@@ -1,10 +1,13 @@
+from bananas.appointments.models import ScheduledMessage
+from datetime import datetime
 from django.conf import settings
 import requests
+from twilio.rest import TwilioRestClient
 
 
 def send_email(
-    subject='Bananas Appointment',
     body,
+    subject='Bananas Appointment',
     to_email='Tara Bartholomew <tara@bananasbunch.org>',
     from_email='Tara Bartholomew <tara@bananasbunch.org>'
 ):
@@ -22,13 +25,42 @@ def send_email(
     )
 
 twilio_client = TwilioRestClient(
-    settings.TWILIO['TWILIO_ACCOUNT_SID'],
-    settings.TWILIO['TWILIO_AUTH_TOKEN']
+    settings.TWILIO['account_sid'],
+    settings.TWILIO['auth_token']
 )
 
 def send_sms(to_number, body):
     message = twilio_client.messages.create(
         body=body,
         to=to_number,
-        from_=settings.TWILIO['TWILIO_PHONE_NUMBER']
+        from_=settings.TWILIO['phone_number']
     )
+
+def send_messages():
+    messages_to_send = ScheduledMessage.objects.filter(time__lte=datetime.now())
+    sent_messages = 0
+    failed_messages = 0
+    for message in messages_to_send:
+        if message.appointment.client_phone:
+            # Send to twilio
+            try:
+                send_sms(
+                    body=message.text,
+                    to_number=message.appointment.client_phone
+                )
+                sent_messages += 1
+            except:
+                failed_messages += 1
+
+        elif message.appointment.client_email:
+            # Send to mailgun
+            try:
+                send_email(
+                    body=message.text,
+                    to_email=message.appointment.client_email
+                )
+                send_messages += 1
+            except:
+                failed_messages += 1
+        message.delete()
+    return (sent_messages, failed_messages)
