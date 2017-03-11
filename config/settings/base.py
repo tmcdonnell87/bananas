38e60fc1c806452f1df3ce514d673619ddf3c6e2
Gaurav <gaurav@gauravkulkarni.com>
2017-03-11 14:12:00 -0800

    Remove celery

--- config/settings/base.py
+++ config/settings/base.py
@@ -261,16 +261,6 @@ LOGIN_URL = 'account_login'
 # SLUGLIFIER
 AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
 
-########## CELERY
-INSTALLED_APPS += ['bananas.taskapp.celery.CeleryConfig']
-BROKER_URL = env('CELERY_BROKER_URL', default='django://')
-if BROKER_URL == 'django://':
-    CELERY_RESULT_BACKEND = 'redis://'
-else:
-    CELERY_RESULT_BACKEND = BROKER_URL
-########## END CELERY
-
-
 # Location of root django.contrib.admin URL, use {% url 'admin:index' %}
 ADMIN_URL = r'^admin/'
 
